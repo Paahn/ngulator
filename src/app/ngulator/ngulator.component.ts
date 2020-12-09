2a1ec9305c0f29d74771d7de750c6e8e96cfa46c
Panos Hatzinikolaou <pjhatzis@gmail.com>
2020-12-08 22:14:40 -0800

    logging operator on click succesfully, screen coming up as undefined for now

--- src/app/ngulator/ngulator.component.ts
+++ src/app/ngulator/ngulator.component.ts
@@ -12,4 +12,15 @@ export class NgulatorComponent{
   clearButton: string = 'clear';
   numbers: string[] = ['9', '0', '5', '6', '7', '8', '1', '2', '3', '4'];
   operators: string[] = ['+', '-', '*', '/'];
+
+  setOperator(value: string) {
+    this.screen(value);
+    this.operator = value;
+    console.log(this.operator);
+    console.log(this.screen(value));
+  }
+
+  screen(expression) {
+    this.result += expression.toString();
+  }
 }

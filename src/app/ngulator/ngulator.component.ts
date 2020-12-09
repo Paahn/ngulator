d922ef36e1cf05f14d31c424496a8505241dfd5c
Panos Hatzinikolaou <pjhatzis@gmail.com>
2020-12-08 22:00:22 -0800

    adding main calculator component to app view

--- src/app/ngulator/ngulator.component.ts
+++ src/app/ngulator/ngulator.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-ngulator',
+  selector: 'ngulator',
   templateUrl: './ngulator.component.html',
   styleUrls: ['./ngulator.component.css']
 })

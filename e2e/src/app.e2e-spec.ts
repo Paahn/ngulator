8ecb015d68b5990eb13887f2b4efe06eb18ded02
Panos Hatzinikolaou <pjhatzis@gmail.com>
2020-12-10 21:06:51 -0800

    renamed the application to ngulator

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to bcch-angular-exercise!');
+    expect(page.getTitleText()).toEqual('Welcome to ngulator!');
   });
 
   afterEach(async () => {
